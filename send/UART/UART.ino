c03f15f02ab860cadaf1ac91d960d9425c8c591d
Witters,Bas B.L.F.A <b.witters@student.fontys.nl>
2018-05-16 12:46:51 +0200

    Send is working not full duplex _again_

--- send/UART/UART.ino
+++ send/UART/UART.ino
@@ -1,7 +1,7 @@
-// heck
+// Send working
 
 #define sizeOfSerializedByte 12
-#define sizeOfReceivedByte 11 // for receiving, the startbit won't be read into the buffer.
+#define sizeOfReceivedByte 11 // for receiving, the startbit wont be counted into the program.
 #define sampleAmount 8
 
 #define parityLocation 9
@@ -36,63 +36,46 @@ const unsigned int sendPin = 13;
 const unsigned int recvPin = 3;
 
 bool sending = false;
+bool received = false;  // Did we receive something to analyse & decode?
+bool flushRecvBuffer = false; // Flushing the buffer?
+
 int currentBit = 0;
+int ReceivedBitSample = 0;
 
+// Settings
 const unsigned int bitRate = 9600; // (bitRate must be >= 1 and < 65536)
-const unsigned int parityMode = oddParityMode;
-const unsigned int stopBits = oneStopbit;
-
+const unsigned int parityMode = evenParityMode;
+const unsigned int stopBits = twoStopbits;
 
 long interruptFreq = 16000000 / bitRate / sampleAmount;
 
-int ReceivedBitSample = 0;
-
 unsigned char serializedByte[sizeOfSerializedByte];
-unsigned char receivedByte[sizeOfReceivedByte];
+unsigned char startBitBuffer[sampleAmount];
+unsigned char receivedByteBuffer[sizeOfReceivedByte][sampleAmount];
+bool byteBufferFilled = false;
 unsigned char bufferByte = '\0';
 
+int bytePlace = 0;
+int samplePlace = 0;
+int tempBit;
+int ReceivedByte;
+
+int receiveSwitch = 0;
 int messageSize = 0;
+
+// Used for checking bits
+const int amountOfSamplesUsed = sampleAmount / 4;
+unsigned char usedSamples[amountOfSamplesUsed];
+int startOfUsedSamplesInBuffer = (sampleAmount / 2 - (sampleAmount / 8));
 
 ISR(TIMER1_COMPA_vect) // Interrupt service routine: Timer 1 matches desired count.
 {
-  if (sending)
-  {
-    if (messageSize > currentBit)
-    {
-      if (ReceivedBitSample == sampleAmount - 1) // MagicNumber.exe
-      {
-        if (serializedByte[currentBit] == HIGH)
-        {
-          // Set third bit from the right to HIGH
-          digitalWrite(sendPin, HIGH);
-        }
-        else
-        {
-          // Disable third bit from the right
-          digitalWrite(sendPin, LOW);
-        }
-        currentBit++;
-        ReceivedBitSample = 0;
-      }
-      else
-      {
-        ReceivedBitSample++;
-      }
-    }
-    else
-    {
-      sending = false;
-      currentBit = 0;
-      ReceivedBitSample = 0;
-    }
-  }
-  else
-  {
-    digitalWrite(sendPin, HIGH);
-  } 
+  // The combination of the two appears to be too slow to function.
+  UARTsend();
+  // UARTreceive();
 }
 
-void setup() 
+void setup()
 {
   pinMode(sendPin, OUTPUT);
   pinMode(recvPin, INPUT);
@@ -110,20 +93,31 @@ void setup()
 
   TCCR1B |= (1 << WGM12); // Turn on Clear Timer on Compare match mode
 
-  // Set CS10 bits for 1 prescaler
+  // Set CS12, CS11 and CS10 bits for 1 prescaler
   TCCR1B |= (1 << CS10);
 
   TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt
 
   sei(); // Allow interrupts
+
+  Serial.begin(9600);
 }
 
-void loop() 
+void loop()
 {
   if (!sending && Serial.available() > 0)
   {
     bufferByte = Serial.read();
     Sending(bufferByte);
+  }
+
+  //byteReading();
+  if (received)
+  {
+    //Serial.print("ReceivedByte ");
+    Serial.println(ReceivedByte);
+    ReceivedByte = 0;
+    receiveSwitch = flushing;
   }
 }
 
@@ -131,7 +125,7 @@ void clearBuffer(int bufferSize, unsigned char* array)
 {
   for (int i = 0; i < bufferSize; ++i)
   {
-    // Set all of the bits in the buffer to 0.
+    // Set all of the bits in the serialized byte buffer to 0.
     array[i] = 0;
   }
 }
